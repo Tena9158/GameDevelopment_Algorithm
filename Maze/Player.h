9189b6a94a3fe903bf851e81d419c98fa40ee2d5
Tenaci <jon1004kkqwe@gmail.com>
2024-01-31 21:21:31 +0900

    PathFinding by 'Right Hand On Wall' Algorithm

--- Maze/Player.h
+++ Maze/Player.h
@@ -3,20 +3,28 @@ class Board;
 
 class Player
 {
-
+	enum
+	{
+		MOVE_TICK = 100
+	};
 public:
-	void	Init(Board* board);
-	void	Update(uint64 deltaTick);
+	void		Init(Board* board);
+	void		Update(uint64 deltaTick);
 
+	bool		CanGo(Pos pos);
 	
 public:
-	Pos		GetPos() { return _pos; }
-	void	SetPosition(Pos pos) { _pos = pos; }
+	Pos			GetPos() { return _pos; }
+	void		SetPosition(Pos pos) { _pos = pos; }
 
 private:
-	Pos		_pos = { };
-	int32	_dir = DIR_UP;
-	Board*	_board = nullptr;
+	Pos			_pos = { };
+	int32		_dir = DIR_UP;
+	Board*		_board = nullptr;
+
+	vector<Pos> _path; // 경로 저장 벡터
+	uint32		_pathindex; // 경로 인덱스
+	uint64		_sumTick = 0;
 
 };
 
